20c91029fbab419cc1d9415b62f527e7dd17b1b8
Robin <r.vangrinsven@isprojects.nl>
2021-04-19 15:20:22 +0200

    delete default_render_class

--- testapp/settings.py
+++ testapp/settings.py
@@ -49,9 +49,6 @@ TEMPLATES = [
 ROOT_URLCONF = "testapp.urls"
 
 REST_FRAMEWORK = {
-    'DEFAULT_RENDERER_CLASSES': (
-        'drf_ujson.renderers.UJSONRenderer',
-    ),
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 100,
     "TEST_REQUEST_DEFAULT_FORMAT": "json",
